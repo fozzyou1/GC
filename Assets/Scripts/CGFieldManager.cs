969ee6867ea472036c73871d8898bb92e6ef6ad9
Park <fozzyou1@naver.com>
2016-05-19 23:31:48 +0900

    초기화

---
+++ Assets/Scripts/CGFieldManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CGFieldManager : MonoBehaviour {
+
+	// 저 에디터가 너무 구려서 그래요
+
+	void Start () {
+		
+	}
+	
+
+	void Update () {
+	
+	}
+
+	
+}
